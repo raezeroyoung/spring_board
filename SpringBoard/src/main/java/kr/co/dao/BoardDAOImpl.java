e0410088b003397697e4a09d779be403a819244c
Leedawoon <leegandhi18@gmail.com>
2020-08-30 01:32:45 +0900

    게시판 글 목록 조회

--- SpringBoard/src/main/java/kr/co/dao/BoardDAOImpl.java
+++ SpringBoard/src/main/java/kr/co/dao/BoardDAOImpl.java
@@ -1,5 +1,7 @@
 package kr.co.dao;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.apache.ibatis.session.SqlSession;
@@ -12,12 +14,20 @@ public class BoardDAOImpl implements BoardDAO {
 
 	@Inject
 	private SqlSession sqlSession;
-	
+
 	// 게시글 작성
 	@Override
 	public void write(BoardVO boardVO) throws Exception {
 		sqlSession.insert("boardMapper.insert", boardVO);
-		
+
+	}
+
+	// 게시물 목록 조회
+	@Override
+	public List<BoardVO> list() throws Exception {
+
+		return sqlSession.selectList("boardMapper.list");
+
 	}
 
 }
--- SpringBoard/src/main/java/kr/co/service/BoardServiceImpl.java
+++ SpringBoard/src/main/java/kr/co/service/BoardServiceImpl.java
@@ -1,5 +1,6 @@
 package kr.co.service;
 
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -13,11 +14,17 @@ public class BoardServiceImpl implements BoardService {
 
 	@Inject
 	private BoardDAO dao;
-	
+
 	// 게시글 작성
 	@Override
 	public void write(BoardVO boardVO) throws Exception {
 		dao.write(boardVO);
 	}
 
+	// 게시물 목록 조회
+	@Override
+	public List<BoardVO> list() throws Exception {
+
+		return dao.list();
+	}
 }
