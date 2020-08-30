04a5d756d46b90cd147ba1b1dbfeb55dd70d87fa
Leedawoon <leegandhi18@gmail.com>
2020-08-30 18:44:48 +0900

    게시글 검색 및 부트스트랩 적용

--- SpringBoard/src/main/java/kr/co/dao/BoardDAOImpl.java
+++ SpringBoard/src/main/java/kr/co/dao/BoardDAOImpl.java
@@ -8,7 +8,7 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
 
 import kr.co.vo.BoardVO;
-import kr.co.vo.Criteria;
+import kr.co.vo.SearchCriteria;
 
 @Repository
 public class BoardDAOImpl implements BoardDAO {
@@ -25,17 +25,17 @@ public class BoardDAOImpl implements BoardDAO {
 
 	// 게시물 목록
 	@Override
-	public List<BoardVO> list(Criteria cri) throws Exception {
+	public List<BoardVO> list(SearchCriteria scri) throws Exception {
 
-		return sqlSession.selectList("boardMapper.listPage", cri);
+		return sqlSession.selectList("boardMapper.listPage", scri);
 
 	}
 
 	// 게시물 총 갯수
 	@Override
-	public int listCount() throws Exception {
+	public int listCount(SearchCriteria scri) throws Exception {
 
-		return sqlSession.selectOne("boardMapper.listCount");
+		return sqlSession.selectOne("boardMapper.listCount", scri);
 
 	}
 
